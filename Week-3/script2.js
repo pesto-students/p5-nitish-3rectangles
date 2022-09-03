38b81af820e8f55ecff386fdd2fb13bb260038be
3rectangles <99025657+3rectangles@users.noreply.github.com>
2022-09-03 21:55:04 +0530

    bind function for ques 2

---
+++ Week-3/script2.js
@@ -0,0 +1,18 @@
+// bind  
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + 'I choose you!');
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+
+var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+
+logPokemon('sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
