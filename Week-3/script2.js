4f0e40155f2ab95ea61de2a7e0312adf643c219c
3rectangles <99025657+3rectangles@users.noreply.github.com>
2022-09-03 21:55:43 +0530

    csall and  apply, in similar example as bind

--- Week-3/script2.js
+++ Week-3/script2.js
@@ -16,3 +16,21 @@ var pokemonName = function(snack, hobby) {
 var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
 
 logPokemon('sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
+
+// CALL and APPLY
+
+var pokemon1 = {
+    firstname: 'Rai',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName1 = function(snack, hobby) {
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+
+pokemonName1.call(pokemon1,'sushi', 'algorithms'); // Rai Chu  loves sushi and algorithms
+pokemonName1.apply(pokemon1,['sushi', 'algorithms']); // Rai Chu  loves sushi and algorithms
